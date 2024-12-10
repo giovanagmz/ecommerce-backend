c9cf8877789e967ad471794f92259d34f8ce3b46
giovanagmz <giovanagimenez55@gmail.com>
2024-12-10 20:24:11 -0300

    push

--- src/main/java/org/senac/aula01/controller/ProdutoController.java
+++ src/main/java/org/senac/aula01/controller/ProdutoController.java
@@ -1,22 +1,22 @@
 package org.senac.aula01.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 import org.senac.aula01.model.Produto;
 import org.senac.aula01.respository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "*") 
 @RequestMapping("/produto")
 public class ProdutoController {
 
@@ -24,16 +24,29 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @GetMapping
-    public List<Produto> get(
-        @RequestParam(required = false, defaultValue = "nome") String order,
-        @RequestParam(required = false) String filter
-    ) {
+    public List<Produto> get(@RequestParam(required = false, defaultValue = "nome") String order,
+                            @RequestParam(required = false) String filter) {
         Sort s = Sort.by(order);
+        switch (order) {
+            case "valor_mais_alto":
+                s = Sort.by(Sort.Order.desc("preco"));
+                break;
+            case "valor_mais_baixo":
+                s = Sort.by(Sort.Order.asc("preco"));
+                break;
+            case "nome":
+            default:
+                s = Sort.by(Sort.Order.asc("nome"));
+                break;
+        }
+   
         if (filter == null) {
             return repository.findAll(s);
         }
         return repository.findByNomeContains(filter, s);
-    }    
+   
+ 
+    }
 
     @PostMapping
     public Produto save(@RequestBody Produto produto) {
